63dfeb975588014443371b6d07996f839961634c
huybkdn23 <huybkdn23@gmail.com>
2018-09-08 20:54:56 +0700

    update

---
+++ xuatjs.js
@@ -0,0 +1,10 @@
+if(confirm("Bạn có muốn xóa trang?") == true)
+{
+    alert("Deleted");
+}
+else
+{
+    alert("Done");
+}
+var input   = prompt("Please enter your name:", "Enter your fuckin name in there.");
+alert(input);
